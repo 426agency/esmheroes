088de4a71a8d56957834dd0b17427481c58e8871
fbihack <info@dhdev.com>
2011-04-20 15:26:03 +0200

    nice set up

---
+++ src/it/unibz/connectors/Bugzilla.java
@@ -0,0 +1,27 @@
+package it.unibz.connectors;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+
+
+public class Bugzilla {
+public static void main(String[] args) throws MalformedURLException, XmlRpcException {
+ XmlRpcClientConfigImpl  config = new XmlRpcClientConfigImpl();
+ config.setServerURL(new URL("https://bugzilla.mozilla.org/xmlrpc.cgi"));
+
+ XmlRpcClient client = new XmlRpcClient();
+ client.setConfig(config);
+
+ Map map = new HashMap();
+ map.put("login","info@dhdev.com");
+ map.put("password","thehons88");
+
+ Map result = (Map) client.execute("User.login",  new Object[]{map});
+ System.out.println("Result = "+result);
+}
+}
