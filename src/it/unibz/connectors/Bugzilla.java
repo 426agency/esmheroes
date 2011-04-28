2336ec5e134e4d9fe5694e2dc54bc87876a77842
fbihack <info@dhdev.com>
2011-04-29 00:12:48 +0200

    bugzilla nearly done

--- src/it/unibz/connectors/Bugzilla.java
+++ src/it/unibz/connectors/Bugzilla.java
@@ -13,51 +13,72 @@ import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 
-
+//http://www.bugzilla.org/docs/3.2/en/html/api/
 public class Bugzilla {
-	public static void main(String[] args) throws MalformedURLException, XmlRpcException {
-		XmlRpcClientConfigImpl  config = new XmlRpcClientConfigImpl();
 
-		config.setServerURL(new URL("https://bugs.kde.org/xmlrpc.cgi"));
+	private static XmlRpcClientConfigImpl  config=null;
+	private static XmlRpcClient client=null;
+	private static Map map = null;
+	
+	private static void init(){
+		if(config==null){
+			config = new XmlRpcClientConfigImpl();
+			try {
+				config.setServerURL(new URL("https://bugs.kde.org/xmlrpc.cgi"));
+				client = new XmlRpcClient();
+				client.setConfig(config);
+				map = new HashMap();
+				map.put("login","info@dhdev.com");
+				map.put("password","thehons88");
+				client.execute("User.login",  new Object[]{map});
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 
-		XmlRpcClient client = new XmlRpcClient();
-		client.setConfig(config);
-
-		Map map = new HashMap();
-		map.put("login","info@dhdev.com");
-		map.put("password","thehons88");
-		client.execute("User.login",  new Object[]{map});
+		}
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static long getDelay(boolean exclusive, List<String> heroes){
+		long ret=0;
+		init();
+	//bug_status=ASSIGNED&bug_status=REOPENED&bug_severity=critical&bug_severity=grave&bug_severity=major&bug_severity=crash&bug_severity=normal&bug_severity=minor&votes=21&order=bugs.votes
 		map = new HashMap();
+		map.put("ids", new String[]{"*"});
 
-		//bug_status=ASSIGNED&bug_status=REOPENED&bug_severity=critical&bug_severity=grave&bug_severity=major&bug_severity=crash&bug_severity=normal&bug_severity=minor&votes=21&order=bugs.votes
-		map.put("status", "CLOSED");
-		map.put("status", "VERIFIED");
-		map.put("severity", "critical");
-		map.put("resolution", "FIXED");
-		map.put("op_sys", "LINUX");
-		Map result = (Map) client.execute("Bug.search",  new Object[]{map});
+		Map result;
+		try {
+			result = (Map) client.execute(new String("Bug.get"),  new Object[]{map});
+		} catch (XmlRpcException e) {
+			// TODO Auto-generated catch block
+return 0;
+}
 
-		//creation_time
-		// dateTime The time the attachment was created.
-		//last_change_time
-		//dateTime The last time the attachment was modified.
 		List l = Arrays.asList((Object[])result.get("bugs"));
-		long delay=0;
 		//Now we have our list of bugs, lets sum delaytimes = completation time
 		int s =l.size();
 		for(int i=0;i<s;i++){
 			HashMap hm = (HashMap)l.get(i);
-			delay+=((Date)hm.get("last_change_time")).getTime()-((Date)hm.get("creation_time")).getTime();
+			if(exclusive&&heroes.contains((String)hm.get("assgined_to"))||
+					(!exclusive&&!heroes.contains((String)hm.get("assgined_to"))))
+				ret+=((Date)hm.get("last_change_time")).getTime()-((Date)hm.get("creation_time")).getTime();
 		}
-		String res= String.format("%d hours, %d min, %d sec", 
-				TimeUnit.MILLISECONDS.toHours(delay),
-				TimeUnit.MILLISECONDS.toMinutes(delay)-
-				TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(delay)),
-				TimeUnit.MILLISECONDS.toSeconds(delay) - 
-				TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(delay))
-		);
+//		String res= String.format("%d hours, %d min, %d sec", 
+//				TimeUnit.MILLISECONDS.toHours(delay),
+//				TimeUnit.MILLISECONDS.toMinutes(delay)-
+//				TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(delay)),
+//				TimeUnit.MILLISECONDS.toSeconds(delay) - 
+//				TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(delay))
+//		);
+		return ret;
+	}
+	
+	public static void main(String[] args) throws MalformedURLException, XmlRpcException {
+		init();
 
-		System.out.println("Total delay time = "+res);
+		
+
+		System.out.println("Total delay time = "+getDelay(true,new ArrayList<String>()));
 
 	}
 }
